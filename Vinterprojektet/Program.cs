108bfeb1766dbe08f301cc2bec577c5171c9cfc5
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-11-11 10:25:21 +0100

    First Commit

---
+++ Vinterprojektet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vinterprojektet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
