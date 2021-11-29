56501f56bdd66c7f2265549c4f5d88fadc689865
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-11-29 19:23:42 +0100

    Kommentarer

--- Vinterprojektet/Food.cs
+++ Vinterprojektet/Food.cs
@@ -9,22 +9,19 @@ namespace Vinterprojektet
 
         int yFood = 0;
         int xFood = 0;
-        Random generator = new Random();
         public int score = 0;
         public int worth = 0;
 
-        public Rectangle foodPosition = new Rectangle(100, 100, 40, 40);
+        Random generator = new Random();
+        public Rectangle foodPosition = new Rectangle(100, 100, 40, 40); //Matens storlek och postition 
 
         public void updateFoodPosition()
         {
-            xFood = generator.Next(20, 980);
-            yFood = generator.Next(20, 580);
+            xFood = generator.Next(20, 980);        //Maten får en random x
+            yFood = generator.Next(20, 580);        //Maten får en random x
             foodPosition.x = xFood;
             foodPosition.y = yFood;
-            worth = generator.Next(20, 100);
+            worth = generator.Next(20, 100);        // Worth blir random
         }
-
-
-
     }
 }
